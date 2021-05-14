5920dfc30082f006f29f14fb9f5d5b860c77f7c1
Dmitry <ilchenko-dima@mail.ru>
2021-05-14 20:48:11 +0300

    add script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 "use strict";
 
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
+console.log(1 + +'5');
 
-const user = 'Dima';
-alert(`Привет, ${user}`);
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2 + 2 * 2 !== '8');
+
+const isChecked = true,
+        isClose = false;
+
+console.log(isChecked || !isClose);
